0169e0e21315b6be5d7a063316d6463a19118fb4
anniebombanie <42328163+anniebombanie@users.noreply.github.com>
2021-06-12 21:12:39 +0000

    Deploying to gh-pages from  @ d8ff6dd1bdfe2e26481c3b43c13ad49a5ac521b5 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a simple mini-site to explore Next.js' new features (based on the starter template for [Learn Next.js](https://nextjs.org/learn)) + Github Actions.
--- next.config.js
+++
@@ -1,6 +0,0 @@
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
-
-module.exports = {
-  // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix: ghPages ? '/anniebombanie.github.io/' : ''
-};
