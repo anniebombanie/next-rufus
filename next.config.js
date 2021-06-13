6e0db006406b59022c35c1a103fe35cbccd27a72
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:27:54 -0400

    test with statically.io

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 // const ghPages = process.env.NODE_ENV === 'production';
 // const basePath = ghPages ? '/next-rufus' : '';
 
+// module.exports = {
+//   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
+//   assetPrefix: 'https://anniebombanie.github.io/next-rufus/'
+// };
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix: 'https://anniebombanie.github.io/next-rufus/'
-};
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/anniebombanie.github.io/next-rufus/' : '',
+}
