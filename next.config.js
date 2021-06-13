6f4462e6647c131d5029bcd87a95edb703514aae
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:13:19 -0400

    remove basePath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ out// const ghPages = process.env.NODE_ENV === 'production';
 // const basePath = ghPages ? '/next-rufus' : '';
 
 module.exports = {
-  basePath: '/next-rufus',
   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
   assetPrefix: 'https://anniebombanie.github.io/next-rufus/'
 };
