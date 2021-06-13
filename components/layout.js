3209fa960f92c01966d273ac24d3689b8dce2371
Annie Liew <anniebombanie@gmail.com>
2021-06-12 21:57:55 -0400

    another update

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { prefix } from '../utils/prefix.js';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const ghPages = process.env.NODE_ENV === 'production';
+const basePath = ghPages ? '/next-rufus' : '';
 
 module.exports = {
+  basePath,
   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix: ghPages ? 'https://anniebombanie.github.io/next-rufus/' : '',
-  basePath:'/next-rufus'
+  assetPrefix: ghPages ? 'https://anniebombanie.github.io/next-rufus/' : ''
 };
