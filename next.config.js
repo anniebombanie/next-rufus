349b7fa35403541e46e4df726a05032e8c51da49
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:29:46 -0400

    update statically link

--- next.config.js
+++ next.config.js
@@ -10,5 +10,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/anniebombanie.github.io/next-rufus/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/anniebombanie/anniebombanie.github.io/next-rufus/' : '',
 }
