d10cdffce127d2691a1f89709ab174fc44397594
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:06:38 -0400

    remove logic from assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ const basePath = ghPages ? '/next-rufus' : '';
 module.exports = {
   basePath,
   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix: ghPages ? 'anniebombanie.github.io/next-rufus/' : ''
+  assetPrefix:'anniebombanie.github.io/next-rufus/'
 };
