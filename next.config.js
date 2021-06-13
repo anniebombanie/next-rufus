3d78de4c462a0adfbbc94f4ab5a5927255b94968
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:39:49 -0400

    trying another one

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-const ghPages = process.env.NODE_ENV === 'production';
-const basePath = ghPages ? '/next-rufus' : '';
-const assetPrefix = ghPages ? 'anniebombanie.github.io/next-rufus/' : '';
+// const ghPages = process.env.NODE_ENV === 'production';
+// const basePath = ghPages ? '/next-rufus' : '';
+// const assetPrefix = ghPages ? 'anniebombanie.github.io/next-rufus/' : '';
+
+// module.exports = {
+//   basePath,
+//   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
+//   assetPrefix
+// };
 
 module.exports = {
-  basePath,
-  // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix
-};
+  assetPrefix: '/next-rufus/',
+}
