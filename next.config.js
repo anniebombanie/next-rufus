55446bac7a24acd1605ca8657648ea1f75e847cc
Annie Liew <anniebombanie@gmail.com>
2021-06-12 20:06:59 -0400

    update config again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  basePath: ghPages ? '/next-rufus' : '',
+  basePath: '/next-rufus',
   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
   assetPrefix: ghPages ? '/anniebombanie.github.io/next-rufus/' : ''
 };
