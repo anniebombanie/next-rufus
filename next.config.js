70c7c968a99310e5eadb96a2ced1a2cc77376521
Annie Liew <anniebombanie@gmail.com>
2021-06-12 22:11:47 -0400

    commented out const ghPages from next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const ghPages = process.env.NODE_ENV === 'production';
+out// const ghPages = process.env.NODE_ENV === 'production';
 // const basePath = ghPages ? '/next-rufus' : '';
 
 module.exports = {
   basePath: '/next-rufus',
   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-  assetPrefix: 'anniebombanie.github.io/next-rufus/'
+  assetPrefix: 'https://anniebombanie.github.io/next-rufus/'
 };
