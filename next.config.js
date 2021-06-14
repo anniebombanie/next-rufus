97e1bb3cc3c0403df4d540651bd6aa5f8562d412
Annie Liew <anniebombanie@gmail.com>
2021-06-13 20:15:27 -0400

    clean up next.config.js & github-actions.yml

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// const ghPages = process.env.NODE_ENV === 'production';
-// const basePath = ghPages ? '/next-rufus' : '';
-// const assetPrefix = ghPages ? 'anniebombanie.github.io/next-rufus/' : '';
-
-// module.exports = {
-//   basePath,
-//   // In production (gh-pages), static assets point to <username>.github.io/<projectname>
-//   assetPrefix
-// };
-
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
