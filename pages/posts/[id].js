1b09680d92367041f8e592bbbba5c5783e436fff
anniebombanie <anniebombanie@gmail.com>
2022-04-21 16:51:48 -0400

    get hero image to work in posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,24 +1,25 @@
-import Head from 'next/head';
-import Date from '../../components/date';
-import Layout from '../../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts';
-import utilStyles from '../../styles/utils.module.css';
+import Head from "next/head";
+import Date from "../../components/date";
+import Layout from "../../components/layout";
+import { prefix } from '../../utils/prefix';
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
@@ -32,8 +33,17 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
+        {postData.img ? (
+          <img
+            src={`${prefix}/imgs/${postData.img.src}`}
+			width='100%'
+			height='auto'
+            alt={postData.img.src}
+			className={utilStyles.postHeroImg}
+          />
+        ) : null}
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  )
+  );
 }
